3096bc3898cd78c7f0820eaac304a3b23184bc6d
Mahmoud Nada <mahmoudn@google.com>
2022-06-28 13:52:31 +0000

    ImageImport_drop_rhel_6_e2e_test

--- cli_tools_tests/e2e/gce_image_import_export/test_suites/import/import_tests.go
+++ cli_tools_tests/e2e/gce_image_import_export/test_suites/import/import_tests.go
@@ -258,10 +258,6 @@ var basicCases = []*testCase{
 		source:   "projects/compute-image-import-test/global/images/centos-8-3",
 		os:       "centos-8",
 	}, {
-		caseName: "el-rhel-6-10",
-		source:   "projects/compute-image-import-test/global/images/rhel-6-10",
-		os:       "rhel-6",
-	}, {
 		caseName:  "el-rhel-7-uefi",
 		source:    "projects/compute-image-import-test/global/images/linux-uefi-no-guestosfeature-rhel7",
 		os:        "rhel-7",
