6ef3e12488e2f837c9cd57505001505da404fb48
Mahmoud Nada <mahmoudn@google.com>
2022-06-30 16:08:16 +0000

    OVF_Import_change_e2e_test_instance_of_ubuntu_1_18_with_LVM_GMA_test

--- cli_tools_tests/e2e/gce_ovf_import/test_suites/ovf_machine_image_import/ovf_machine_image_import_test_suite.go
+++ cli_tools_tests/e2e/gce_ovf_import/test_suites/ovf_machine_image_import/ovf_machine_image_import_test_suite.go
@@ -221,7 +221,7 @@ func runOVFMachineImageImportUbuntu18WithBootDiskSpanMultiplePhysicalDisksWithLV
 			Zone:                  "us-west1-c",
 			ExpectedStartupOutput: "All tests passed!",
 			FailureMatches:        []string{"FAILED:", "TestFailed:"},
-			SourceURI:             fmt.Sprintf("gs://%v/ova/ubuntu-18-boot-disk-spans-with-lvm", ovaBucket),
+			SourceURI:             fmt.Sprintf("gs://%v/ova/ubuntu-18-instance-with-lvm", ovaBucket),
 			Os:                    "ubuntu-1804",
 			MachineType:           "n1-standard-4",
 		}}
