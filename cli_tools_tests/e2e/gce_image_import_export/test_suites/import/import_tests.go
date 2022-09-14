8e1104157c5f464f8997d65a5f0f09c2b648c76e
Mahmoud Nada <94056180+MahmoudNada0@users.noreply.github.com>
2022-09-14 14:37:32 +0200

    ImageImport_drop_e2e_tests_that_tests_importing_centos8 (#62)

--- cli_tools_tests/e2e/gce_image_import_export/test_suites/import/import_tests.go
+++ cli_tools_tests/e2e/gce_image_import_export/test_suites/import/import_tests.go
@@ -254,14 +254,6 @@ var basicCases = []*testCase{
 		source:   "projects/compute-image-import-test/global/images/centos-7-8",
 		os:       "centos-7",
 	}, {
-		caseName: "el-centos-8-0",
-		source:   "projects/compute-image-import-test/global/images/centos-8-import",
-		os:       "centos-8",
-	}, {
-		caseName: "el-centos-8-3",
-		source:   "projects/compute-image-import-test/global/images/centos-8-3",
-		os:       "centos-8",
-	}, {
 		caseName:  "el-rhel-7-uefi",
 		source:    "projects/compute-image-import-test/global/images/linux-uefi-no-guestosfeature-rhel7",
 		os:        "rhel-7",
